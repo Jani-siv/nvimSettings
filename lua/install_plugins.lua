efbe0ee90f261d4975421683c2593f1fd33ef5d0
Gyy <54612629+Jani-siv@users.noreply.github.com>
2024-09-17 22:18:58 +0300

    Update install_plugins.lua

--- lua/install_plugins.lua
+++ lua/install_plugins.lua
@@ -1,6 +1,6 @@
 vim.cmd([[
   augroup PlugInstall
     autocmd!
-    autocmd VimEnter * PlugInstall --sync | source $MYVIMRC | qall
+    autocmd VimEnter * PlugInstall --sync | source ~/.config/nvim/init.lua | qall
   augroup END
 ]])
